605cbfc45289edbee4c4ff5d8ae930a3f6d65c4d
tracer-ics2015 <tracer@njuics.org>
2015-09-22 23:36:12 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-28-generic #30-Ubuntu SMP Mon Aug 31 15:52:10 UTC 2015 i686 i686 i686 GNU/Linux  23:36:12 up  4:29,  2 users,  load average: 0.58, 0.47, 0.38 d2e02fd058bedeed117ae636b8d0e234131de64b

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -1,5 +1,5 @@
-
 #include "nemu.h"
+#include "unistd.h"
 
 #define ENTRY_START 0x100000
 
@@ -14,80 +14,90 @@ void init_ddr3();
 
 FILE *log_fp = NULL;
 
-static void init_log() {
-	log_fp = fopen("log.txt", "w");
-	Assert(log_fp, "Can not open 'log.txt'");
+static void init_log()
+{
+    log_fp = fopen("log.txt", "w");
+    Assert(log_fp, "Can not open 'log.txt'");
 }
 
-static void welcome() {
-	printf("Welcome to NEMU!\nThe executable is %s.\nFor help, type \"help\"\n",
-			exec_file);
+static void welcome()
+{
+    printf("Welcome to NEMU!\nThe executable is %s.\nFor help, type \"help\"\n",
+           exec_file);
 }
 
-void init_monitor(int argc, char *argv[]) {
-	/* Perform some global initialization */
+void init_monitor(int argc, char *argv[])
+{
+    /* Perform some global initialization */
 
-	/* Open the log file. */
-	init_log();
+    /* Open the log file. */
+    init_log();
 
-	/* Load the string table and symbol table from the ELF file for future use. */
-	load_elf_tables(argc, argv);
+    /* Load the string table and symbol table from the ELF file for future use. */
+    load_elf_tables(argc, argv);
 
-	/* Compile the regular expressions. */
-	init_regex();
+    /* Compile the regular expressions. */
+    init_regex();
 
-	/* Initialize the watchpoint link list. */
-	init_wp_list();
+    /* Initialize the watchpoint link list. */
+    init_wp_list();
 
-	/* Display welcome message. */
-	welcome();
+    /* Display welcome message. */
+    welcome();
 }
 
 #ifdef USE_RAMDISK
-static void init_ramdisk() {
-	int ret;
-	const int ramdisk_max_size = 0xa0000;
-	FILE *fp = fopen(exec_file, "rb");
-	Assert(fp, "Can not open '%s'", exec_file);
+static void init_ramdisk()
+{
+    int ret;
+    const int ramdisk_max_size = 0xa0000;
+    FILE *fp = fopen(exec_file, "rb");
+    Assert(fp, "Can not open '%s'", exec_file);
 
-	fseek(fp, 0, SEEK_END);
-	size_t file_size = ftell(fp);
-	Assert(file_size < ramdisk_max_size, "file size(%zd) too large", file_size);
+    fseek(fp, 0, SEEK_END);
+    size_t file_size = ftell(fp);
+    Assert(file_size < ramdisk_max_size, "file size(%zd) too large", file_size);
 
-	fseek(fp, 0, SEEK_SET);
-	ret = fread(hwa_to_va(0), file_size, 1, fp);
-	assert(ret == 1);
-	fclose(fp);
+    fseek(fp, 0, SEEK_SET);
+    ret = fread(hwa_to_va(0), file_size, 1, fp);
+    assert(ret == 1);
+    fclose(fp);
 }
 #endif
 
-static void load_entry() {
-	int ret;
-	FILE *fp = fopen("entry", "rb");
-	Assert(fp, "Can not open 'entry'");
+static void load_entry()
+{
+    char buf[80];
+//    getcwd(buf, sizeof(buf));
+    printf("current working directory: %s\n", buf);
 
-	fseek(fp, 0, SEEK_END);
-	size_t file_size = ftell(fp);
+    int ret;
+    FILE *fp = fopen("entry", "rb");
+    Assert(fp, "Can not open 'entry'");
 
-	fseek(fp, 0, SEEK_SET);
-	ret = fread(hwa_to_va(ENTRY_START), file_size, 1, fp);
-	assert(ret == 1);
-	fclose(fp);
+    fseek(fp, 0, SEEK_END);
+    size_t file_size = ftell(fp);
+
+    fseek(fp, 0, SEEK_SET);
+    ret = fread(hwa_to_va(ENTRY_START), file_size, 1, fp);
+    assert(ret == 1);
+    fclose(fp);
 }
 
-void restart() {
-	/* Perform some initialization to restart a program */
+void restart()
+{
+    /* Perform some initialization to restart a program */
 #ifdef USE_RAMDISK
-	/* Read the file with name `argv[1]' into ramdisk. */
-	init_ramdisk();
+    /* Read the file with name `argv[1]' into ramdisk. */
+    init_ramdisk();
 #endif
 
-	/* Read the entry code into memory. */
-	load_entry();
+    /* Read the entry code into memory. */
+    load_entry();
 
-	/* Set the initial instruction pointer. */
-	cpu.eip = ENTRY_START;
+    /* Set the initial instruction pointer. */
+    cpu.eip = ENTRY_START;
 
-	/* Initialize DRAM. */
-	init_ddr3();
+    /* Initialize DRAM. */
+    init_ddr3();
 }
