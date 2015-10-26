0145bd164977ff9a49e905429db1ca403829468d
tracer-ics2015 <tracer@njuics.org>
2015-10-26 15:04:39 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  15:04:39 up  1:04,  2 users,  load average: 0.21, 0.20, 0.23 80a935b6fb17f52658ffa1354922db3d5ccbd5b8

--- nemu/src/cpu/exec/logic/sub.h
+++ nemu/src/cpu/exec/logic/sub.h
@@ -1,15 +1,15 @@
 #ifndef __SUB_H__
 #define __SUB_H__
 
-make_helper(sub_i2a_b);
-make_helper(sub_i2rm_b);
-make_helper(sub_r2rm_b);
-make_helper(sub_rm2r_b);
+make_helper(sub_i2a_b);//2c
+make_helper(sub_i2rm_b);//80  group problem
+make_helper(sub_r2rm_b);//28
+make_helper(sub_rm2r_b);//2a
 
-make_helper(sub_i2a_v);
-make_helper(sub_i2rm_v);
-make_helper(sub_si2rm_v);
-make_helper(sub_r2rm_v);
-make_helper(sub_rm2r_v);
+make_helper(sub_i2a_v);//2d
+make_helper(sub_i2rm_v);//81  group
+make_helper(sub_si2rm_v);//83 group
+make_helper(sub_r2rm_v);//29
+make_helper(sub_rm2r_v);//2b
 
 #endif
