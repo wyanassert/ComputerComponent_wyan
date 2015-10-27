efbe19ec6e790588b8611a9cc0fa4ac60ca34835
tracer-ics2015 <tracer@njuics.org>
2015-10-27 10:33:12 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  10:33:12 up  2:28,  2 users,  load average: 0.43, 0.30, 0.32 c95befd02503cefafafe3b476388eaf08af244d6

--- nemu/src/cpu/exec/logic/test.h
+++ nemu/src/cpu/exec/logic/test.h
@@ -2,11 +2,11 @@
 #define __TEST_H__
 
 make_helper(test_i2a_b); //a8
-make_helper(test_i2rm_b); //f6 group
+make_helper(test_i2rm_b); //f6 /0 group Cpl
 make_helper(test_r2rm_b); //84
 
 make_helper(test_i2a_v); //a9
-make_helper(test_i2rm_v); //f7 group
+make_helper(test_i2rm_v); //f7 /0 group Cpl
 make_helper(test_r2rm_v); //85
 
 #endif
