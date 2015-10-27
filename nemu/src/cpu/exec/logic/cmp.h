67c1f8328e3cf2259e839cc8098012b273745f57
tracer-ics2015 <tracer@njuics.org>
2015-10-27 10:26:23 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  10:26:23 up  2:21,  2 users,  load average: 0.32, 0.30, 0.32 7aa7f91a7ad3b3d1468eee1197dc908d7c1faccf

--- nemu/src/cpu/exec/logic/cmp.h
+++ nemu/src/cpu/exec/logic/cmp.h
@@ -2,13 +2,13 @@
 #define __CMP_H__
 
 make_helper(cmp_i2a_b);//3c
-make_helper(cmp_i2rm_b);//80
+make_helper(cmp_i2rm_b);//80 group
 make_helper(cmp_r2rm_b); //38
 make_helper(cmp_rm2r_b); //3a
 
 make_helper(cmp_i2a_v);//3d
-make_helper(cmp_i2rm_v);//81
-make_helper(cmp_si2rm_v);//83
+make_helper(cmp_i2rm_v);//81 group
+make_helper(cmp_si2rm_v);//83 group
 make_helper(cmp_r2rm_v); //39
 make_helper(cmp_rm2r_v); //3b
 
