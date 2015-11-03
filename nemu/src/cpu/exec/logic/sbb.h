103109869199629564851075e1032b5d2538886d
tracer-ics2015 <tracer@njuics.org>
2015-11-03 20:50:29 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  20:50:29 up  1:27,  2 users,  load average: 0.07, 0.17, 0.22 a6018e8af8fe3cabbaf752eb7dbef82c85d21842

---
+++ nemu/src/cpu/exec/logic/sbb.h
@@ -0,0 +1,16 @@
+#ifndef SBB_H_INCLUDED
+#define SBB_H_INCLUDED
+
+make_helper(sbb_i2a_b); //1c
+make_helper(sbb_i2rm_b);  //80 3
+make_helper(sbb_r2rm_b);  //18
+make_helper(sbb_rm2r_b);  //1a
+
+make_helper(sbb_i2a_v);  //1d
+make_helper(sbb_i2rm_v);  //81 3
+make_helper(sbb_si2rm_v); //83 3
+make_helper(sbb_r2rm_v); //19
+make_helper(sbb_rm2r_v); //1b
+
+
+#endif // SBB_H_INCLUDED
