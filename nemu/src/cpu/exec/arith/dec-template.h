6d2f8924f87424259fecbd772476d7fbdbced48b
tracer-ics2015 <tracer@njuics.org>
2015-11-04 22:17:47 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  22:17:47 up  1:37,  2 users,  load average: 0.23, 0.29, 0.37 7385ee0e7ad277ef73136b8e1ae3fbb156470bb2

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -6,8 +6,20 @@ static void do_execute () {
 	DATA_TYPE result = op_src->val - 1;
 	OPERAND_W(op_src, result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	/* TODO: Update  */
+	cpu.PF = (result ^ (result >> 1) ^ (result >> 2) ^ (result >> 3) ^ (result >> 4) ^ (result >> 5) ^ (result >> 6) ^ (result >> 7)) & 0x01;
+    cpu.AF = ((uint32_t)(op_src ->val & 0x0f) < (uint32_t)(1 & 0x0f));
+	cpu.ZF = (result == 0);
+#if DATA_BYTE == 1
+    cpu.SF = (result >> 7) & 0x1;
+#endif // DATA_BYTE
+#if DATA_BYTE == 2
+    cpu.SF = (result >> 15) & 0x1;
+#endif // DATA_BYTE
+#if DATA_BYTE == 4
+    cpu.SF = (result >> 31) & 0x1;
+#endif // DATA_BYTE
+	cpu.OF = ((int32_t)(op_src->val) < 0) && ((int32_t)result > 0);
 
 	print_asm_template1();
 }
