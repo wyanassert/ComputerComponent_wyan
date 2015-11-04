a79f636b247a01bb112164dd2f44ba2eb912595e
tracer-ics2015 <tracer@njuics.org>
2015-11-04 22:14:43 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-31-generic #36-Ubuntu SMP Wed Oct 7 15:03:41 UTC 2015 i686 i686 i686 GNU/Linux  22:14:43 up  1:34,  2 users,  load average: 0.23, 0.33, 0.40 24ed84b98ff9b66edc93ba34bf84a667133f11b7

--- nemu/src/cpu/exec/arith/inc.h
+++ nemu/src/cpu/exec/arith/inc.h
@@ -1,9 +1,8 @@
 #ifndef __INC_H__
 #define __INC_H__
 
-make_helper(inc_rm_b);
-
-make_helper(inc_rm_v);
-make_helper(inc_r_v);
+make_helper(inc_rm_b);//fe 0
+make_helper(inc_rm_v);//ff 0
+make_helper(inc_r_v); //40
 
 #endif
