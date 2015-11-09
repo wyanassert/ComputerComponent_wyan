d180a603e820d6f6eabf274e2eec1303d142ead0
tracer-ics2015 <tracer@njuics.org>
2015-11-09 23:09:06 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-32-generic #37-Ubuntu SMP Wed Oct 21 10:22:32 UTC 2015 i686 i686 i686 GNU/Linux  23:09:06 up 51 min,  3 users,  load average: 0.18, 0.22, 0.30 9e43039c6f745d8d280696390f9b2b8e1e0832d

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -4,6 +4,7 @@
 #include "data-mov/xchg.h"
 #include "data-mov/movsx.h"
 #include "data-mov/movzx.h"
+#include "data-mov/movs.h"
 
 #include "arith/dec.h"
 #include "arith/inc.h"
---
+++ nemu/src/cpu/exec/data-mov/movs.h
@@ -0,0 +1,9 @@
+#ifndef MOVS_H_INCLUDED
+#define MOVS_H_INCLUDED
+
+#include "cpu/exec/helper.h"
+
+make_helper(movsb);
+make_helper(movsv);
+
+#endif // MOVS_H_INCLUDED
