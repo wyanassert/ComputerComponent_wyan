bfc78d3c884664ff85bdafdf1d850a7531bbea1e
tracer-ics2015 <tracer@njuics.org>
2015-11-16 17:32:19 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-33-generic #38-Ubuntu SMP Fri Nov 6 18:17:18 UTC 2015 i686 i686 i686 GNU/Linux  17:32:19 up  1:10,  2 users,  load average: 0.49, 0.43, 0.45 456a979913e07e1f31788d1f11df5f5651578f7d

---
+++ nemu/src/cpu/exec/jcc/jcxz-template.h
@@ -0,0 +1,21 @@
+#include "cpu/exec/template-start.h"
+
+#define instr jcxz
+
+static void do_execute ()
+{
+    if (cpu.ecx == 0)
+    {
+        int32_t tsrc = (DATA_TYPE_S)op_src->val;
+        cpu.eip += tsrc;
+#if DATA_BYTE == 2
+        cpu.eip &= 0x0000ffff;
+#endif // DATA_BYTE
+    }
+    print_asm_template1();
+}
+
+make_instr_helper(i)
+
+
+#include "cpu/exec/template-end.h"
