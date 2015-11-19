ad25b1795b9d36ffb884cfe9ca5fee053cb67614
tracer-ics2015 <tracer@njuics.org>
2015-11-19 09:04:39 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-33-generic #38-Ubuntu SMP Fri Nov 6 18:17:18 UTC 2015 i686 i686 i686 GNU/Linux  09:04:39 up 36 min,  3 users,  load average: 0.32, 0.37, 0.41 293be7e261ac6969385ee163fef10cbdd25d112

--- nemu/src/cpu/exec/logic/je-template.h
+++ nemu/src/cpu/exec/logic/je-template.h
@@ -3,12 +3,17 @@
 #define instr je
 
 static void do_execute(){
-    uint32_t result = cpu.eip + (int32_t)op_src->val;
-    if(DATA_BYTE == 2){
-    	cpu.eip = result & 0x0000FFFF;
+    if(cpu.ZF==1){
+        #if DATA_BYTE==1
+            cpu.eip+=(DATA_TYPE_S)op_src->val;
+        #elif DATA_BYTE==2
+            cpu.eip=cpu.eip+(DATA_TYPE_S)op_src->val;
+            cpu.eip=cpu.eip&0xFFFF;
+        #else
+            cpu.eip+=(int32_t)op_src->val;
+        #endif // DATA_BYTE
     }
-    else
-        cpu.eip = result;
+
 	print_asm_template1();
 }
 
