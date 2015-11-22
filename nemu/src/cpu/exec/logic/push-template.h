08f2b8989c3bd1cc7267e6939e78b245ff8c95a7
tracer-ics2015 <tracer@njuics.org>
2015-11-22 10:37:49 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-33-generic #38-Ubuntu SMP Fri Nov 6 18:17:18 UTC 2015 i686 i686 i686 GNU/Linux  10:37:49 up 45 min,  3 users,  load average: 0.48, 0.35, 0.30 3d66f3b0bd05f569ee878d8abc04a944bccb24c

--- nemu/src/cpu/exec/logic/push-template.h
+++ nemu/src/cpu/exec/logic/push-template.h
@@ -4,21 +4,22 @@
 
 static void do_execute(){
 
-  #if DATA_BYTE==1
-    cpu.esp-=2;
-    MEM_W(cpu.esp,op_src->val);
-    #else
-    cpu.esp-=4;
-    MEM_W(cpu.esp,op_src->val);
-    #endif
-
+#if DATA_BYTE == 2
+    cpu.esp -= 2;
+    swaddr_write(cpu.esp, 2, op_src->val);
+#else
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, op_src->val);
+#endif // DATA_BYTE
     print_asm_template1();
 
 
 }
 
-make_instr_helper(r)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(rm)
+make_instr_helper(r)
+#endif // DATA_BYTE
 make_instr_helper(i)
 
 
