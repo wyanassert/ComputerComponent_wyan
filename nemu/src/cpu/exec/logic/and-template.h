dacf1206816117ee4ffd70dfa3b296bcf58a0a74
tracer-ics2015 <tracer@njuics.org>
2015-11-22 11:10:34 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-33-generic #38-Ubuntu SMP Fri Nov 6 18:17:18 UTC 2015 i686 i686 i686 GNU/Linux  11:10:34 up  1:17,  3 users,  load average: 0.16, 0.29, 0.30 ff04e9982ba6439151dddd46c0d4e7343723cae0

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -3,8 +3,7 @@
 #define instr and
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val & (int32_t)op_src->val;
-	printf("%d  %x\n",(int32_t)op_src->val, op_dest->val);
+	DATA_TYPE result = op_dest->val & op_src->val;
     OPERAND_W(op_dest, result);
 
     cpu.CF = 0;
@@ -12,12 +11,15 @@ static void do_execute () {
 
 #if DATA_BYTE == 1
     cpu.SF = (result >> 7) & 0x1;
+    printf("1\n");
 #endif // DATA_BYTE
 #if DATA_BYTE == 2
     cpu.SF = (result >> 15) & 0x1;
+    printf("2\n");
 #endif // DATA_BYTE
 #if DATA_BYTE == 4
     cpu.SF = (result >> 31) & 0x1;
+    printf("4\n");
 #endif // DATA_BYTE
     cpu.ZF = (result == 0);
     cpu.PF = (result ^ (result >> 1) ^ (result >> 2) ^ (result >> 3) ^ (result >> 4) ^ (result >> 5) ^ (result >> 6) ^ (result >> 7)) & 0x01;
