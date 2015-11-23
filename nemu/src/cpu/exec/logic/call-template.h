44115d23af90a75500c48c1b8d0037854b54c369
tracer-ics2015 <tracer@njuics.org>
2015-11-23 09:37:30 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-33-generic #38-Ubuntu SMP Fri Nov 6 18:17:18 UTC 2015 i686 i686 i686 GNU/Linux  09:37:30 up  1:28,  3 users,  load average: 0.51, 0.45, 0.30 f33dd322cb47ae4432864dbc54c87074132691f3

--- nemu/src/cpu/exec/logic/call-template.h
+++ nemu/src/cpu/exec/logic/call-template.h
@@ -4,7 +4,20 @@
 
 static void do_execute()
 {
-    #if DATA_BYTE == 2
+    int len=concat(decode_i_, SUFFIX)(cpu.eip + 1) + 1;
+	if(2 == DATA_BYTE){
+		cpu.esp =cpu.esp - 2;
+		MEM_W(cpu.esp, (uint16_t)((cpu.eip + len) & 0x0000ffff));
+
+		cpu.eip = (cpu.eip + op_src->val)&0x0000ffff;
+	}
+	else if(4 == DATA_BYTE){
+		cpu.esp = cpu.esp-4;
+		MEM_W(cpu.esp,  cpu.eip + len);
+		cpu.eip = cpu.eip+ op_src->val;
+	}
+	print_asm_template1();
+   /* #if DATA_BYTE == 2
     cpu.esp -= 4;
     MEM_W(cpu.esp, cpu.eip);
     cpu.eip = (op_src->val + cpu.eip) & 0x0000ffff;
@@ -13,7 +26,7 @@ static void do_execute()
     MEM_W(cpu.esp, cpu.eip);
     cpu.eip+=op_src->val;
     #endif // DATA_BYTE
-    print_asm_template1();
+    print_asm_template1();*/
 }
 
 make_instr_helper(i)
