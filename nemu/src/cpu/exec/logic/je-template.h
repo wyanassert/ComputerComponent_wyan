8fbf3a4ba32c9278b184e337b7039268e26675f6
tracer-ics2015 <tracer@njuics.org>
2015-11-23 14:24:57 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-33-generic #38-Ubuntu SMP Fri Nov 6 18:17:18 UTC 2015 i686 i686 i686 GNU/Linux  14:24:57 up 22 min,  3 users,  load average: 0.24, 0.34, 0.30 835adad0cfe68c855ca52834650f375cbd4b4e87

--- nemu/src/cpu/exec/logic/je-template.h
+++ nemu/src/cpu/exec/logic/je-template.h
@@ -3,17 +3,14 @@
 #define instr je
 
 static void do_execute(){
-    if(cpu.ZF==1){
-        #if DATA_BYTE==1
-            cpu.eip+=(DATA_TYPE_S)op_src->val;
-        #elif DATA_BYTE==2
-            cpu.eip=cpu.eip+(DATA_TYPE_S)op_src->val;
-            cpu.eip=cpu.eip&0xFFFF;
-        #else
-            cpu.eip+=(int32_t)op_src->val;
-        #endif // DATA_BYTE
+    if (cpu.ZF)
+    {
+        int32_t tsrc = (DATA_TYPE_S)op_src->val;
+        cpu.eip += tsrc;
+#if DATA_BYTE == 2
+        cpu.eip &= 0x0000ffff;
+#endif // DATA_BYTE
     }
-
 	print_asm_template1();
 }
 
--- nemu/src/cpu/exec/logic/jmp-template.h
+++ nemu/src/cpu/exec/logic/jmp-template.h
@@ -5,9 +5,23 @@
 
 static void do_execute(){
 
-   cpu.eip+=op_src->val;
-   print_asm_template1();
+   if (op_src->type == OP_TYPE_IMM)
+    {
+        int32_t tsrc = (DATA_TYPE_S)op_src->val;
+        cpu.eip += tsrc;
+#if DATA_BYTE == 2
+        cpu.eip &= 0x0000ffff;
+#endif // DATA_BYTE
+    }
+    else
+    {
+        cpu.eip = op_src->val;
+#if DATA_BYTE == 2
+        cpu.eip &= 0xffff;
+#endif // DATA_BYTE
 
+    }
+    print_asm_template1();
 
 }
 
