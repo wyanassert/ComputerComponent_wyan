cd4b25e7b2f18a2e8ef8d8c404851559aca20f5d
tracer-ics2015 <tracer@njuics.org>
2015-11-23 14:21:41 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-33-generic #38-Ubuntu SMP Fri Nov 6 18:17:18 UTC 2015 i686 i686 i686 GNU/Linux  14:21:41 up 19 min,  3 users,  load average: 0.44, 0.30, 0.27 f6b657d897b8293ff5fff5878d7e70522145ab8b

--- nemu/src/cpu/exec/logic/add-template.h
+++ nemu/src/cpu/exec/logic/add-template.h
@@ -6,68 +6,23 @@ static void do_execute () {
 	DATA_TYPE result = op_dest->val + op_src->val;
 	OPERAND_W(op_dest, result);
 
-	int dest=(int)op_dest->val;
-	int src=(int)op_src->val;
-	int result_flag=dest+src;
-	int PF_flag=0;
+	cpu.CF = ((uint32_t)(result) < (uint32_t)(op_src->val));
+	cpu.PF = (result ^ (result >> 1) ^ (result >> 2) ^ (result >> 3) ^ (result >> 4) ^ (result >> 5) ^ (result >> 6) ^ (result >> 7)) & 0x01;
+    cpu.AF = ((uint32_t)(result & 0x0f) < (uint32_t)(op_src->val & 0x0f));
+	cpu.ZF = (result == 0);
+#if DATA_BYTE == 1
+    cpu.SF = (result >> 7) & 0x1;
+#endif // DATA_BYTE
+#if DATA_BYTE == 2
+    cpu.SF = (result >> 15) & 0x1;
+#endif // DATA_BYTE
+#if DATA_BYTE == 4
+    cpu.SF = (result >> 31) & 0x1;
+#endif // DATA_BYTE
+	cpu.OF = (((int32_t)(op_dest->val) > 0) && ((int32_t)(op_src->val) > 0) && ((int32_t)result < 0)) || (((int32_t)(op_dest->val) < 0) && ((int32_t)(op_src->val) < 0) && ((int32_t)result > 0));
 
 
-	if(result>0)
-        cpu.SF=0;
-    else
-        if(result<=0)
-            cpu.SF=1;
-
-   if(result==0){
-        cpu.ZF=1;
-    }else cpu.ZF=0;
-
-    if((dest>0&&src>0&&result_flag<0)||(dest<0&&src<0&&result_flag>0))
-        cpu.OF=1;
-    else cpu.OF=0;
-
-    #if DATA_BYTE==1
-    if((unsigned)op_dest->val+(unsigned)(op_src->val)>=256)
-        cpu.CF=1;
-    else
-        cpu.CF=0;
-    #elif DATA_BYTE==2
-    if((unsigned)op_dest->val+(unsigned)(op_src->val)>=65536)
-        cpu.CF=1;
-    else cpu.CF=0;
-    #elif DATA_BYTE==4
-    if((unsigned)op_dest->val+(unsigned)(op_src->val)<=4294967296)
-        cpu.CF=1;
-    else cpu.CF=0;
-    #endif // DATA_BYTE
-
-
-    if(result&0x01)
-        PF_flag++;
-    if(result&0x02)
-        PF_flag++;
-    if(result&0x04)
-        PF_flag++;
-    if(result&0x08)
-        PF_flag++;
-    if(result&0x10)
-        PF_flag++;
-    if(result&0x20)
-        PF_flag++;
-    if(result&0x40)
-        PF_flag++;
-    if(result&0x80)
-        PF_flag++;
-    if(PF_flag%2==0)
-       cpu.PF=1;
-    else cpu.PF=0;
-
-   if((unsigned)(op_dest->val&0xF)<(unsigned)((-op_src->val)&0xF))
-        cpu.AF=1;
-    else cpu.AF=0;
-
-
-
+	print_asm_template2();
 
 	print_asm_template2();
 }
