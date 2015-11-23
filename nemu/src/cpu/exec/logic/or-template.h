4e5ecbdc8386c0588ad6f95dc63b060168c153b3
tracer-ics2015 <tracer@njuics.org>
2015-11-23 09:45:29 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 3.19.0-33-generic #38-Ubuntu SMP Fri Nov 6 18:17:18 UTC 2015 i686 i686 i686 GNU/Linux  09:45:29 up  1:36,  3 users,  load average: 0.22, 0.25, 0.25 58c72add67167f346afe35e8cc1a8c0cccaa4618

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -4,10 +4,21 @@
 
 static void do_execute () {
 	DATA_TYPE result = op_dest->val | op_src->val;
-	OPERAND_W(op_dest, result);
+    OPERAND_W(op_dest, result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+    cpu.OF = 0;
+    cpu.CF = 0;
+#if DATA_BYTE == 1
+    cpu.SF = (result >> 7) & 0x1;
+#endif // DATA_BYTE
+#if DATA_BYTE == 2
+    cpu.SF = (result >> 15) & 0x1;
+#endif // DATA_BYTE
+#if DATA_BYTE == 4
+    cpu.SF = (result >> 31) & 0x1;
+#endif // DATA_BYTE
+    cpu.ZF = (result == 0);
+    cpu.PF = (result ^ (result >> 1) ^ (result >> 2) ^ (result >> 3) ^ (result >> 4) ^ (result >> 5) ^ (result >> 6) ^ (result >> 7)) & 0x01;
 
 	print_asm_template2();
 }
