941cd4051fd0be7777fc3892a665075027d3fbfd
tracer-ics2015 <tracer@njuics.org>
2016-01-07 16:33:50 +0800

    >  compile kernel 141220000 wyan Linux ubuntu 4.2.0-23-generic #28-Ubuntu SMP Sun Dec 27 17:48:35 UTC 2015 i686 i686 i686 GNU/Linux  16:33:50 up  2:21,  5 users,  load average: 0.02, 0.16, 0.31 7db7ea1def6d2a99f0898fda8baedaa6b01abdb2

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -38,11 +38,10 @@ void init() {
 
 /* Initialization phase 2 */
 void init_cond() {
-	nemu_assert(0);
 #ifdef IA32_INTR
 	/* Reset the GDT, since the old GDT in start.S cannot be used in the future. */
 	init_segment();
-
+	nemu_assert(0);
 	/* Set the IDT by setting up interrupt and exception handlers.
 	 * Note that system call is the only exception implemented in NEMU.
 	 */
