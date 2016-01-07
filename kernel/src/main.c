2187f7f7fef938d56be7669f78119b478f9b3ce1
tracer-ics2015 <tracer@njuics.org>
2016-01-07 16:50:46 +0800

    >  compile kernel 141220000 wyan Linux ubuntu 4.2.0-23-generic #28-Ubuntu SMP Sun Dec 27 17:48:35 UTC 2015 i686 i686 i686 GNU/Linux  16:50:46 up  2:38,  5 users,  load average: 0.05, 0.09, 0.19 e76c1f2ef8b3d32e590cf2871958c9f9bba62561

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -83,7 +83,7 @@ void init_cond() {
 	/* Load the program. */
 	//nemu_assert(0);
 	uint32_t eip = loader();
-	nemu_assert(0);
+	
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
 	 * the test data is written sucessfully.
