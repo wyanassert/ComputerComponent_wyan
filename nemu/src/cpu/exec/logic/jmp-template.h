3b9796eb227cd5f33dcb79ca318e25462c261225
tracer-ics2015 <tracer@njuics.org>
2016-01-07 19:22:21 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 4.2.0-23-generic #28-Ubuntu SMP Sun Dec 27 17:48:35 UTC 2015 i686 i686 i686 GNU/Linux  19:22:21 up  1:38,  5 users,  load average: 0.23, 0.28, 0.29 561043729b430b0c97bf6dbafb43bf1352d6550

--- nemu/src/cpu/exec/logic/jmp-template.h
+++ nemu/src/cpu/exec/logic/jmp-template.h
@@ -24,7 +24,15 @@ static void do_execute(){
     print_asm_template1();
 
 }
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_helper(concat5(instr, _, rm, _, SUFFIX))
+{
+    concat4(decode_, rm, _, SUFFIX)(eip+1);
+    do_execute();
+    return 0;
+}
+#endif // DATA_BYTE
 
 make_instr_helper(i)
-make_instr_helper(rm);
+//make_instr_helper(rm);
 #include "cpu/exec/template-end.h"
