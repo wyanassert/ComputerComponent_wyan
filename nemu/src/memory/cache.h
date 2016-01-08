f9bf93ce1a7090484fba0ad5b863c5e3621b527b
tracer-ics2015 <tracer@njuics.org>
2016-01-08 21:48:38 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 4.2.0-23-generic #28-Ubuntu SMP Sun Dec 27 17:48:35 UTC 2015 i686 i686 i686 GNU/Linux  21:48:38 up  1:58,  3 users,  load average: 0.58, 0.36, 0.33 1588b4a78a97edd4395414e6336cd25a88457e11

--- nemu/src/memory/cache.h
+++ nemu/src/memory/cache.h
@@ -1,4 +1,24 @@
 #ifndef _CACHE_H
 #define _CACHE_H
 
+#include "common.h"
+
+typedef struct cache_block{
+	bool valid;
+	uint32_t addr;
+	uint32_t value;
+} CACHE_BLOCK;
+
+typedef struct cache_set
+{
+	CACHE_BLOCK block[8];/* data */
+} CACHE_SET;
+
+typedef struct cache
+{
+	CACHE_SET set[128];/* data */
+} CACHE;
+
+CACHE cache;
+
 #endif
