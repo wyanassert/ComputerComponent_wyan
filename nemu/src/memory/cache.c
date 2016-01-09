41642dc118bb20d825f0fd576af927a912235fed
tracer-ics2015 <tracer@njuics.org>
2016-01-09 22:39:06 +0800

    >  compile NEMU 141220000 wyan Linux ubuntu 4.2.0-23-generic #28-Ubuntu SMP Sun Dec 27 17:48:35 UTC 2015 i686 i686 i686 GNU/Linux  22:39:06 up  7:39,  4 users,  load average: 0.15, 0.16, 0.16 9fa97234ddd5937e6c35805f930412b975a084ef

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -76,8 +76,9 @@ void writecache(hwaddr_t addr, size_t len, uint32_t data)
 				cache.set[setnum].block[i].value = data;
 		}
 	}
-	if(readcache(addr, 4) != dram_read(addr, 4))
-		printf("write Error cache addr(%x), len(%d), data(%x), chche(%x), dram(%x) \n", addr, len, data, readcache(addr, 4),dram_read(addr, 4));
+	if(addr == 0x8013b8)
+		if(readcache(addr, 4) != dram_read(addr, 4))
+			printf("write Error cache addr(%x), len(%d), data(%x), chche(%x), dram(%x) \n", addr, len, data, readcache(addr, 4),dram_read(addr, 4));
 	random += 7;
 	if(random > 1543)
 		random -= 1543;
