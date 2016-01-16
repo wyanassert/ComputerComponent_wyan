17e56a8a81e2fb9dbe9fd07928438e14106006ec
wyan <1402099772@qq.com>
2016-01-16 09:14:05 -0800

    remove cache info for PA2 check

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -3,7 +3,7 @@
 
 
 uint32_t dram_read(hwaddr_t, size_t);
-// void dram_write(hwaddr_t, size_t, uint32_t);
+void dram_write(hwaddr_t, size_t, uint32_t);
 
 void writecache(hwaddr_t addr, size_t len, uint32_t data);
 uint32_t readcache(hwaddr_t addr, size_t len);
@@ -11,19 +11,19 @@ uint32_t readcache(hwaddr_t addr, size_t len);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	uint32_t result_dram = dram_read(addr, len) & (~0u >> ((4 - len) << 3)), result_cache = readcache(addr, len) & (~0u >> ((4 - len) << 3));
-	if(result_cache!= result_dram)
-		printf("read [error],addr:%x, len:%d, dram:%x, cache:%x\n", addr, len, result_dram, result_cache);
+	// uint32_t result_dram = dram_read(addr, len) & (~0u >> ((4 - len) << 3)), result_cache = readcache(addr, len) & (~0u >> ((4 - len) << 3));
+	// if(result_cache!= result_dram)
+	// 	printf("read [error],addr:%x, len:%d, dram:%x, cache:%x\n", addr, len, result_dram, result_cache);
 	//else
 		//printf("read [success], addr:%x len:%d, dram:%x, cache:%x\n", addr, len, result_dram, result_cache);
-	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	return readcache(addr, len) & (~0u >> ((4 - len) << 3));
+	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	//return readcache(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	//dram_write(addr, len, data);
+	dram_write(addr, len, data);
 	//printf("write: addr:%x, len:%d, data:%x\n", addr, len, data);
-	writecache(addr, len, data);
+	//writecache(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
